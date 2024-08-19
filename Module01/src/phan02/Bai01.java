21c5021517d70b03ecc899495bfefce2d2ce2126
hauutoDesktop <thanhhau670@gmail.com>
2024-08-19 21:14:43 +0700

    vendor: Module01: phan02

---
+++ Module01/src/phan02/Bai01.java
@@ -0,0 +1,26 @@
+package phan02;
+
+
+//Toan tu so hoc
+public class Bai01 {
+    public static void main(String[] args){
+        int a = 7;
+        System.out.println("a = "+a);
+        System.out.println("a + 1 = "+ (a+1));
+        System.out.println("a - 1 = "+ (a-1));
+        System.out.println("a x 3 = "+ (a*3));
+        System.out.println("a / 2 = "+ (a/2));
+        System.out.println("a % 2 = "+ (a%2));
+
+        System.out.println();
+
+        double b = 10;
+        System.out.println("b = "+b);
+        System.out.println("b + 1 = "+ (b+1));
+        System.out.println("b - 1 = "+ (b-1));
+        System.out.println("b x 3 = "+ (b*3));
+        System.out.println("b / 2 = "+ (b/2));
+        System.out.println("b % 2 = "+ (b%2));
+
+    }
+}
---
+++ Module01/src/phan02/Bai06.java
@@ -0,0 +1,15 @@
+package phan02;
+
+//Toan tu dieu kien
+public class Bai06 {
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        boolean condition = a>b;
+        int max =  condition ? a : b;
+
+        System.out.println("a: "+a);
+        System.out.println("b: "+b);
+        System.out.println("Max: "+max);
+    }
+}
