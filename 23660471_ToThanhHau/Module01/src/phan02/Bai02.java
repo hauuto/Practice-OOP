e210ed62d4e6d146a541230fb109f83e0c239b3a
hauutoDesktop <thanhhau670@gmail.com>
2024-09-03 22:30:02 +0700

    add: module01: phan01, phan02

---
+++ 23660471_ToThanhHau/Module01/src/phan02/Bai02.java
@@ -0,0 +1,13 @@
+package phan02;
+
+
+//Phep tinh tren chuoi
+public class Bai02 {
+    public static void main(String[] args){
+        String Ho = "To";
+        String TenDem = " Thanh";
+        String Ten = " Hau";
+        String FullName = Ho + TenDem + Ten;
+        System.out.println(FullName);
+    }
+}
