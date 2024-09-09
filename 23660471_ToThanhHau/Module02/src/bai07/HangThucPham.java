4522d44d98e0d4b94549c9e3773165ec62784e46
hauutoDesktop <thanhhau670@gmail.com>
2024-09-09 18:34:29 +0700

    module 02: bai07

--- 23660471_ToThanhHau/Module02/src/bai07/HangThucPham.java
+++ 23660471_ToThanhHau/Module02/src/bai07/HangThucPham.java
@@ -7,8 +7,10 @@
 
 package bai07;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
-import java.util.Locale;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * @description:
@@ -17,16 +19,16 @@ import java.util.Locale;
  * @created: 9/4/2024 1:56 PM
  */
 public class HangThucPham {
-    private static String maHang;
-    private static String tenHang;
-    private static double donGia;
-    private static LocalDate ngaySanXuat;
-    private static LocalDate ngayHetHan;
+    private String maHang;
+    private String tenHang;
+    private double donGia;
+    private LocalDate ngaySanXuat;
+    private LocalDate ngayHetHan;
 
 
     //Constructors
     public HangThucPham(){
-    	this("xxx","xxx",1,LocalDate.now(),LocalDate.now());
+        this("xxx","xxx",1,LocalDate.now(),LocalDate.now());
     }
 
     public HangThucPham(String maHang, String tenHang, double donGia, LocalDate ngaySanXuat, LocalDate ngayHetHan){
@@ -37,77 +39,89 @@ public class HangThucPham {
         setNgayHetHan(ngayHetHan);
     }
 
+
+
     //Getters and Setters
 
-    public static String getMaHang() {
+    public String getMaHang() {
         return maHang;
     }
 
-    public static void setMaHang(String maHang) {
+    public void setMaHang(String maHang) {
         if (maHang.trim() == "" || maHang == null){
-            throw new IllegalArgumentException("Ma hang khong duoc de trong");
+            throw new IllegalArgumentException("Mã hàng không được trống!");
         }
-        HangThucPham.maHang = maHang;
+        this.maHang = maHang;
     }
 
-    public static String getTenHang() {
+    public String getTenHang() {
         return tenHang;
     }
 
-    public static void setTenHang(String tenHang) {
+    public void setTenHang(String tenHang) {
         if (tenHang.trim() == "" || tenHang == null){
-            HangThucPham.tenHang = "xxx";
+            tenHang = "xxx";
         }
-        HangThucPham.tenHang = tenHang;
+        this.tenHang = tenHang;
     }
 
-    public static double getDonGia() {
+    public double getDonGia() {
         return donGia;
     }
 
-    public static void setDonGia(double donGia) {
+    public void setDonGia(double donGia) {
         if (donGia <= 0){
-            HangThucPham.donGia = 0;
+            donGia = 0;
         }
-        HangThucPham.donGia = donGia;
+        this.donGia = donGia;
     }
 
-    public static LocalDate getNgaySanXuat() {
-    	
+    public LocalDate getNgaySanXuat() {
+
         return ngaySanXuat;
     }
 
-    public static void setNgaySanXuat(LocalDate ngaySanXuat) {
+    public void setNgaySanXuat(LocalDate ngaySanXuat) {
         if (ngaySanXuat.isAfter(LocalDate.now())){
-            HangThucPham.ngaySanXuat = LocalDate.now();
+            ngaySanXuat = LocalDate.now();
         }
-        HangThucPham.ngaySanXuat = ngaySanXuat;
+        this.ngaySanXuat = ngaySanXuat;
     }
 
-    public static LocalDate getNgayHetHan() {
+    public LocalDate getNgayHetHan() {
         return ngayHetHan;
     }
 
-    public static void setNgayHetHan(LocalDate ngayHetHan) {
-    	if (ngayHetHan.isBefore(ngaySanXuat)) {
-    		HangThucPham.ngayHetHan = ngaySanXuat;
-    	}
-        HangThucPham.ngayHetHan = ngayHetHan;
+    public void setNgayHetHan(LocalDate ngayHetHan) {
+        if (ngayHetHan.isBefore(ngaySanXuat)) {
+            ngayHetHan = ngaySanXuat;
+        }
+        this.ngayHetHan = ngayHetHan;
     }
-    
-    
-    
+
+
+
     //Methods
-    public static boolean hetHan() {
-    	if (ngayHetHan.isBefore(LocalDate.now())) {
-    		return true;
-    	}
-    	return false;
+    public boolean hetHan() {
+        if (ngayHetHan.isBefore(LocalDate.now())) {
+            return true;
+        }
+        return false;
     }
-    
-    
+
+    //Format LocalDate to dd/MM/YYYY
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public String ngaySanXuatToString() {
+        return ngaySanXuat.format(dtf);
+    }
+    public String ngayHetHanToString() {
+        return ngayHetHan.format(dtf);
+    }
+
+    DecimalFormat df = new DecimalFormat("#,##0.00");
+
     @Override
-    public static String toString() {
-    	return String.format("%10s %10s %-.2f %15s %15s %",)
+    public String toString() {
+        return String.format("%-10s %-10s %15s %15s %15s %15s",maHang,tenHang,df.format(donGia)+"VND",ngaySanXuatToString(),ngayHetHanToString(),hetHan()==true?"Hàng hết hạn":"");
     }
 }
---
+++ 23660471_ToThanhHau/Module02/src/bai07/TestHangThucPham.java
@@ -0,0 +1,32 @@
+/*
+ * @ (#) TestHangThucPham.java       1.0      9/9/2024
+ *
+ * Copyright (c) 2024 IUH.FIT
+ */
+
+
+package bai07;
+import java.time.LocalDate;
+/**
+ * @description:
+ * @author: To Thanh Hau
+ * @version: 1.0
+ * @created: 9/9/2024 5:19 PM
+ */
+public class TestHangThucPham {
+    public static void main(String[] args) {
+
+        HangThucPham hangThucPham1 = new HangThucPham("001","Gạo",100000, LocalDate.of(2023,9,9),LocalDate.of(2024,9,9));
+        HangThucPham hangThucPham2 = new HangThucPham("002","Mì",20000, LocalDate.of(2023,9,9),LocalDate.of(2024,8,9));
+        HangThucPham hangThucPham3 = new HangThucPham("003","Đường",30000, LocalDate.of(2022,9,9),LocalDate.of(2020,9,9));
+
+
+        System.out.printf("%-10s %-10s %15s %15s %15s %15s \n","Mã Hàng","Tên Hàng","Đơn Giá","Ngày Sản Xuất","Ngày Hết Hạn","Hạn Sử Dụng");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+        System.out.println(hangThucPham1);
+        System.out.println(hangThucPham2);
+        System.out.println(hangThucPham3);
+
+
+    }
+}
