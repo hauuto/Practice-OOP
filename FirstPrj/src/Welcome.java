8ba56b8590632b94e85074da4cb71304b2b5c78b
hauutoDesktop <thanhhau670@gmail.com>
2024-09-17 00:54:42 +0700

    lt2 continue

--- FirstPrj/src/Welcome.java
+++
@@ -1,6 +0,0 @@
-public class Welcome{
-    public static void main(String[] args){
-        System.out.println("Hello To Thanh Hau");
-        System.out.println("422000411904_HK1_2024-2025");
-    }
-}
